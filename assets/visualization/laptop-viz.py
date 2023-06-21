34e50166e3cea004dc10167022db2f1c229bc5d3
novinbukannopin <68802465+novinbukannopin@users.noreply.github.com>
2023-06-21 23:43:13 +0700

    fix adjust rabu malam

--- assets/visualization/laptop-viz.py
+++ assets/visualization/laptop-viz.py
@@ -1,12 +1,15 @@
 import time  #
 
 import matplotlib.pyplot as plt
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np  # import np
 import pandas as pd  # import pd
 import plotly.express as px  # import chart
+import plotly.graph_objects as go
 import streamlit as st
 import altair as alt
+import math
+from PIL import Image
 
 import seaborn as sns
 from pandas import DataFrame
@@ -91,9 +94,6 @@ st.markdown("<hr/>",unsafe_allow_html=True)
 st.markdown("## Visualization Data")
 st.write("")
 
-cccount, col2 = st.columns(2)
-# with cccount:
-colname = ""
 st.subheader("Comparison of amounts ")
 column_count_plot = st.selectbox("Choose a column to plot count. Try Selecting Brand ",laptop.columns)
 colname = column_count_plot
@@ -114,7 +114,6 @@ fig.update_layout(
 
 st.plotly_chart(fig, use_container_width=True)
 
-# with col2:
 st.subheader('Distribution Data')
 option_columns = ["warranty", "msoffice", "os", "windows", "weight", "touchscreen"]
 option_columns_num = [
@@ -143,31 +142,84 @@ fig = px.histogram(
 
 st.plotly_chart(fig, use_container_width=True)
 
-# 10 laptop termahal
-rich_laptop = laptop.sort_values('price', ascending=False, ignore_index=True)
+def scatter_price_laptop(count, title, asc):
+    price_by_laptop = laptop.sort_values('price', ascending=False, ignore_index=True)
+    if(asc == "asc"):
+        clean_price_laptop = price_by_laptop.head(count)
+    elif(asc =="desc"):
+        clean_price_laptop = price_by_laptop.tail(count)
 
-ten_laptop = rich_laptop.head(10)
+    fig = px.scatter(
+        data_frame=clean_price_laptop,
+        x=clean_price_laptop['brand'],
+        y=clean_price_laptop['price'],
+        title=title,
+        color=clean_price_laptop['brand'],
+        hover_name=clean_price_laptop['brand'],
+        hover_data=clean_price_laptop,
+        symbol=clean_price_laptop['brand'],
+        size=clean_price_laptop['price']
+    )
+    st.plotly_chart(fig, use_container_width=True)
 
+st.subheader("Best Pricy Laptop 2022")
+price_one, price_two = st.columns(2)
 
-fig = px.scatter(
-    data_frame=ten_laptop, x=ten_laptop['brand'], y=ten_laptop['price'], title="10 Laptop yang Expensive", color=ten_laptop['brand'], hover_data=ten_laptop, symbol=ten_laptop['brand']
-)
-st.plotly_chart(fig, use_container_width=True)
+with price_one:
+    # 10 laptop termahal
+    scatter_price_laptop(10, "Top 10 Expensive Laptop", asc="asc")
 
-# 10 laptop termurah
-rich_laptop = laptop.sort_values('price', ascending=False, ignore_index=True)
+    # 10 laptop termurah
+with price_two:
+    scatter_price_laptop(10, "Top 10 Cheap Laptop", asc="desc")
 
-ten_laptop_murah = rich_laptop.tail(10)
-
-
-fig = px.scatter(
-    data_frame=ten_laptop_murah, x=ten_laptop_murah['brand'], y=ten_laptop_murah['price'], title="10 Laptop yang Cheap", color=ten_laptop_murah['brand'], hover_data=ten_laptop_murah, symbol=ten_laptop_murah['brand']
-)
-st.plotly_chart(fig, use_container_width=True)
 
 # rating
-laptop["total_rating"] = laptop['rating'] * laptop["no_ratings"]
-best_rating = laptop.sort_values('total_rating', ascending=False, ignore_index=True)
-best_rating_filtered = best_rating[best_rating['rating'] == 5]
-st.table(best_rating_filtered.head(10))
+df = px.data.medals_long()
 
+fig = px.scatter(df, y="nation", x="count", color="medal", symbol="medal", title="Rating")
+fig.update_traces(marker_size=10)
+# st.plotly_chart(fig, use_container_width=True)
+
+# rating
+#
+st.subheader("Best Rating Laptop 2022")
+
+rating_col_1, rating_col_2 = st.columns([3,9])
+with rating_col_1:
+    image = Image.open("../images/laptop-1.jpg")
+    st.write("\n")
+    st.image(image)
+    st.write("Insert amount")
+    color = st.select_slider(
+        'Atur jumlah data yang akan ditampilkan',
+        options=[5, 10, 15, 20, 25, 30, 40, 50], key="rating_1")
+    st.subheader(f"Best {color} \n Laptop Ratings")
+with rating_col_2:
+    rating_5, rating_4, rating_3, rating_2, rating_1 = st.tabs(["Rating :five:", "Rating :four:", "Rating :three:", "Rating :two:", "Rating :one:"])
+
+def dot_plots_rating_laptop(rating, bestnum):
+    rating_filtered = laptop.loc[(laptop['rating']==rating)]
+    sort_rating_price = rating_filtered.sort_values("price", ascending=False)
+    data_rating = sort_rating_price.head(bestnum)
+    fig = px.scatter(data_rating, y="brand", x="price", color="brand", symbol="brand", hover_data=rating_filtered)
+    fig.update_traces(marker_size=10)
+    st.plotly_chart(fig, use_container_width=True)
+
+with rating_5:
+    dot_plots_rating_laptop(rating=5, bestnum=color)
+
+with rating_4:
+    dot_plots_rating_laptop(rating=4, bestnum=color)
+
+with rating_3:
+    dot_plots_rating_laptop(rating=3, bestnum=color)
+
+with rating_2:
+    dot_plots_rating_laptop(rating=2, bestnum=color)
+
+with rating_1:
+    dot_plots_rating_laptop(rating=1, bestnum=color)
+
+
+#
